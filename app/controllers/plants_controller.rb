fdb18f520a4b13234c01980498b16b1adbc1423d
mostlybadfly <reachdanielnunez@gmail.com>
2017-07-10 21:38:55 -0500

    Added plant model, form, and show.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,20 +3,34 @@ class PlantsController < ApplicationController
   end
 
   def show
+    @plant = Plant.find(params[:id])
   end
 
   def new
+    @plant = Plant.new
   end
 
   def edit
   end
 
   def create
+    @plant = Plant.new(plant_params)
+
+    @plant.save
+    redirect_to @plant
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:common_name, :scientific_name,
+                                  :nickname, :origin, :watering_schedule,
+                                  :light_requirement
+                                 )
   end
 end
